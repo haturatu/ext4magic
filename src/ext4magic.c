90d9bb0489cf8ab7e26716453dd9f019c1807763
haturatu <taro@eyes4you.org>
2026-04-20 07:30:00 +0900

    fix: reject unsupported modern ext4 feature flags

--- src/ext4magic.c
+++ src/ext4magic.c
@@ -277,6 +277,46 @@ static void open_filesystem(char *device, int open_flags, blk_t superblock,
                 goto errout;
         }
 
+        if (EXT2_HAS_INCOMPAT_FEATURE(current_fs->super,
+                                      EXT4_FEATURE_INCOMPAT_EA_INODE)) {
+                fprintf(stderr,
+                        "%s uses unsupported ext4 feature: ea_inode\n",
+                        device);
+                goto errout;
+        }
+
+        if (EXT2_HAS_INCOMPAT_FEATURE(current_fs->super,
+                                      EXT4_FEATURE_INCOMPAT_ENCRYPT)) {
+                fprintf(stderr,
+                        "%s uses unsupported ext4 feature: encrypt\n",
+                        device);
+                goto errout;
+        }
+
+        if (EXT2_HAS_INCOMPAT_FEATURE(current_fs->super,
+                                      EXT4_FEATURE_INCOMPAT_CASEFOLD)) {
+                fprintf(stderr,
+                        "%s uses unsupported ext4 feature: casefold\n",
+                        device);
+                goto errout;
+        }
+
+        if (EXT2_HAS_RO_COMPAT_FEATURE(current_fs->super,
+                                       EXT4_FEATURE_RO_COMPAT_VERITY)) {
+                fprintf(stderr,
+                        "%s uses unsupported ext4 feature: verity\n",
+                        device);
+                goto errout;
+        }
+
+        if (EXT2_HAS_COMPAT_FEATURE(current_fs->super,
+                                    EXT4_FEATURE_COMPAT_ORPHAN_FILE)) {
+                fprintf(stderr,
+                        "%s uses unsupported ext4 feature: orphan_file\n",
+                        device);
+                goto errout;
+        }
+
 
         retval = ext2fs_read_inode_bitmap(current_fs);
         if (retval) {
